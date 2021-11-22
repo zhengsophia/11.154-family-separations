46696a5765c5893861d7d736995eec3f0861f77c
Enrique <enriquec@mit.edu>
2021-11-22 15:28:26 -0500

    Optimized arc diagram code and created a temporary modal html file

--- arcdiagram.js
+++ arcdiagram.js
@@ -9,49 +9,92 @@ const barColor = '#ffb752';
 
 const commaFormat = d3.format(',');
 
-const delta = 0.01;
+const ztpStart = new Date(2018, 4, 7);
+const ztpEnd = new Date(2018, 5, 20);
 
 /**
- * Draws the curve
+ * Generates an array of points based on `y = ax^2 + bx + c`.
  *
- * @param {*} d
- * @returns
+ * Adapted from https://jsfiddle.net/uw5akb29/
+ *
+ * @param {number} a - The `a` constant
+ * @param {number} b - The `b` constant
+ * @param {number} c - The `c` constant
+ * @param {number[]} rangeX - X-range array (the domain)
+ * @param {number} step - Step for generation
+ * @returns {number[]} The array of points
  */
-function getCurve(d, bezier, points, t) {
-  var curve = bezier[d];
-  if (!curve) {
-    curve = bezier[d] = [];
-    for (var t_ = 0; t_ <= 1 + delta; t_ += delta) {
-      // Get levels, whatever that means
-      var x = [points.slice(0, d)];
-      for (var i = 1; i < d; i++) {
-        let pt = x[x.length - 1];
-        // Interpolate
-        const nextPoint = [];
-        for (var j = 1; j < pt.length; j++) {
-          var d0 = pt[j - 1],
-            d1 = pt[j];
-          nextPoint.push({
-            x: d0.x + (d1.x - d0.x) * t_,
-            y: d0.y + (d1.y - d0.y) * t_,
-          });
-        }
-        x.push(nextPoint);
-      }
-      curve.push(x[x.length - 1][0]);
-    }
+function createPoints(a, b, c, rangeX, step) {
+  return Array.apply(
+    null,
+    Array(((rangeX[1] - rangeX[0]) / step) | (0 + 1))
+  ).map(function (d, i) {
+    const x = rangeX[0] + i * step;
+    return [x, a * x * x + b * x + c];
+  });
+}
+
+/**
+ * Converts the category index to the category description.
+ *
+ * @param {number} i - The category index
+ * @returns {string} Category description
+ */
+const getCategoryDescription = (i) => {
+  if (i === 0) {
+    return 'Before ZTP';
   }
-  return [curve.slice(0, t / delta + 1)];
-}
+  if (i === 1) {
+    return 'During ZTP';
+  }
 
-function x(d) {
-  return d.x;
-}
-function y(d) {
-  return d.y;
-}
+  return 'After ZTP';
+};
 
-const showCurves = (data) => {
+/**
+ * Converts a date to the category depending on whether it is before the ZTP,
+ * after ZTP, or during.
+ *
+ * @param {Date} date - The date object
+ * @returns {number} The category ID
+ */
+const resolveDateToCategory = (date) => {
+  let id = 0;
+  if (date < ztpStart) {
+    id = 0;
+  } else if (date > ztpEnd) {
+    id = 2;
+  } else {
+    id = 1;
+  }
+  return id;
+};
+
+/**
+ * @typedef Record
+ * @prop {Number} AGE
+ * @prop {String} GENDER - M/F/
+ * @prop {String} COO - Country of origin
+ * @prop {String} DATE_APPREHENDED
+ * @prop {String} REFERRAL_DATE
+ * @prop {String} DATE_ORR_APPROVED
+ * @prop {String} REFERRING_OFFICE
+ * @prop {String} FACILITY_APPROVED
+ * @prop {String} REUNITIED_SEP_PARENTS
+ * @prop {String} UAC_STATUS
+ * @prop {String} Date Discharged
+ * @prop {String} Reason fo Separation
+ * @prop {Number} Duration
+ */
+
+/**
+ * Draws the arc diagram based on the given data.
+ *
+ * @param {Record[]} data - The filtered dataset for each facility
+ */
+const drawArcDiagram = (data) => {
+  console.log(data);
+
   // Create the SVG viewbox
   const svg = d3
     .select('#viz')
@@ -63,131 +106,273 @@ const showCurves = (data) => {
       height + marginTop + marginBottom,
     ]);
 
-  endpoints = data.map((x) => x.end);
+  // Draw the title
+  svg
+    .append('text')
+    .attr('font-family', 'Times New Roman')
+    .attr('font-weight', 100)
+    .attr('font-size', '42px')
+    .attr('fill', '#eeeeee')
+    .attr('text-anchor', 'middle')
+    .attr('x', width / 2)
+    .attr('y', marginTop)
+    .text('Separation Duration');
 
-  // TODO: Update color to show categorical data
+  // Draw the summary text
+  const summaryText = svg.append('g');
+
+  // Total days value
+  summaryText
+    .append('text')
+    .attr('id', 'summary-text-total-days')
+    .attr('font-size', '32px')
+    .attr('fill', '#eeeeee')
+    .attr('x', width - marginRight)
+    .attr('y', marginTop)
+    .text(0);
+
+  // Total days label
+  summaryText
+    .append('text')
+    .attr('font-size', '28px')
+    .attr('fill', '#aaaaaa')
+    .attr('x', width - marginRight)
+    .attr('y', marginTop + 30)
+    .text('total days');
+
+  // Total children value
+  summaryText
+    .append('text')
+    .attr('id', 'summary-text-total-children')
+    .attr('font-size', '32px')
+    .attr('fill', '#eeeeee')
+    .attr('x', width - marginRight)
+    .attr('y', marginTop + 80)
+    .text(0);
+
+  // Total children label
+  summaryText
+    .append('text')
+    .attr('font-size', '28px')
+    .attr('fill', '#aaaaaa')
+    .attr('x', width - marginRight)
+    .attr('y', marginTop + 110)
+    .text('children separated');
+
+  // Styling for summary text
+  summaryText
+    .selectAll('text')
+    .attr('font-family', 'Times New Roman')
+    .attr('text-anchor', 'middle');
+
+  // Extract the endpoints for scaling purposes
+  const endpoints = data.map((x) => x.Duration);
+
+  // Setup colors and draw the legend
   const colors = d3
-    .scaleLinear()
-    .domain(d3.ticks(0, d3.max(endpoints), 5))
-    .range(['#FFFFBF', '#FEE08B', '#FDAE61', '#F46D43', '#D53E4F']);
+    .scaleOrdinal()
+    .domain([0, 1, 2])
+    .range(['#FDAE61', '#D53E4F', '#F46D43']);
 
+  // Draw the legend dots
+  svg
+    .selectAll('legend-dots')
+    .data([0, 1, 2])
+    .enter()
+    .append('circle')
+    .attr('cx', marginLeft)
+    .attr('cy', (d, i) => marginTop + i * 25)
+    .attr('r', 7)
+    .style('fill', colors);
+
+  // Draw the legend labels
+  svg
+    .selectAll('legend-labels')
+    .data([0, 1, 2])
+    .enter()
+    .append('text')
+    .attr('font-family', 'Times New Roman')
+    .attr('x', marginLeft + 20)
+    .attr('y', (d, i) => marginTop + i * 25)
+    .style('fill', '#eeeeee')
+    .text(getCategoryDescription)
+    .attr('text-anchor', 'left')
+    .style('alignment-baseline', 'middle');
+
+  // const colors = d3
+  //   .scaleLinear()
+  //   .domain(d3.ticks(0, d3.max(endpoints), 5))
+  //   .range(['#FFFFBF', '#FEE08B', '#FDAE61', '#F46D43', '#D53E4F']);
+
+  // Setup scales and axes
   const xScale = d3
     .scaleLinear()
-    .domain([0, d3.max(endpoints)])
+    .domain([0, d3.max(endpoints) + 1])
     .range([0, width]);
 
-  const xAxis = d3.axisBottom(xScale).ticks(6);
+  const xAxis = d3.axisBottom(xScale).ticks(10);
 
   const yScale = d3
     .scaleLinear()
     .domain([0, d3.max(endpoints)])
     .range([marginBottom, height - marginTop - 10]);
 
-  svg
-    .selectAll('g')
-    .data(data)
-    .enter()
-    .append('g')
-    .each(function (migrantData) {
-      const stayDuration = migrantData.end;
-
-      // Initialize variables for arc
-      const bezier = {};
-      const line = d3.line().x(x).y(y);
-      const curveHeight = -yScale(stayDuration);
-      const curveLength = xScale(stayDuration);
-      // TODO: Fix points to scale across whole width (use an axis)
-      const points = [
-        { x: marginLeft, y: height - marginBottom },
-        { x: marginLeft + curveLength / 2, y: curveHeight },
-        { x: marginLeft + curveLength, y: height - marginBottom },
-      ];
-      const order = 2;
-      const numControlPoints = order + 1;
-
-      let t = 0;
-      const orders = d3.range(numControlPoints, order + 2);
-
-      // Define the arc in a new group
-      const arc = d3.select(this).selectAll('g').data(orders).enter();
-
-      // Set the speed and start timer for animation
-      // TODO: Adjust rate depending on data
-      var last = 0;
-      let rate = -curveHeight;
-      d3.timer(function (elapsed) {
-        t = t + ((elapsed - last) / rate) * 0.3;
-        last = elapsed;
-        update();
-      });
-
-      // Stop the timer as soon as our parameter exceeds 1
-      if (t > 1) {
-        timer.stop();
-      }
-
-      // TODO: Set the color of the arc
-      const color = colors(stayDuration);
-
-      /**
-       * Updates the curve continuously based on the current parameter `t`.
-       */
-      const update = () => {
-        const curve = arc.selectAll('path').data((d) => {
-          const curveData = getCurve(d, bezier, points, t);
-          return curveData;
-        });
-        // Actually draw the curve
-        curve
-          .enter()
-          .append('path')
-          .attr('stroke', color)
-          .attr('stroke-width', '2px')
-          .attr('fill', 'none')
-
-          // TODO: Tooltip on hover
-          .on('mouseover', function (event, d) {
-            // console.log('mouse entered');
-            d3.select('#tooltip')
-              .style('left', event.pageX + 15 + 'px')
-              .style('top', event.pageY + 'px')
-              .classed('hidden', false);
-            d3.select('#value').text(
-              `Days spent: ${commaFormat(stayDuration)}`
-            );
-            d3.select(this).attr('stroke-width', '3px');
-          })
-          .on('mousemove', (event) => {
-            d3.select('#tooltip')
-              .style('left', event.pageX + 15 + 'px')
-              .style('top', event.pageY + 'px');
-          })
-          .on('mouseout', function () {
-            // console.log('mouse left');
-            d3.select('#tooltip').classed('hidden', true);
-            d3.select(this).attr('stroke-width', '2px');
-          });
-        curve.attr('d', line);
-      };
-
-      arc
-        .append('circle')
-        .attr('class', 'control')
-        .attr('r', 5)
-        .attr('cx', marginLeft + curveLength)
-        .attr('cy', height - marginBottom)
-        .attr('fill', color);
-    });
-
+  // Draw the x-axis
   svg
     .append('g')
     .attr('class', 'x-axis')
     .attr('transform', `translate(${marginLeft}, ${height - marginBottom})`)
     .call(xAxis);
+
+  // Draw the x-axis label
+  svg
+    .append('text')
+    .attr('font-family', 'Times New Roman')
+    .attr('font-weight', 100)
+    .attr('font-size', '28px')
+    .attr('fill', '#aaaaaa')
+    .attr('text-anchor', 'middle')
+    .attr('x', width / 2 + marginLeft)
+    .attr('y', height + marginBottom)
+    .text('days');
+
+  // Define arc constants
+  const circleRadius = 3,
+    circleAnimationDuration = 500,
+    pathStep = 1,
+    pathStroke = 2,
+    pathHoverStroke = 3,
+    pathAnimationDuration = 1000,
+    pathAnimationDelay = (i) => 100 + i * 50; // Stagger the arc animations
+
+  // Draw the actual arcs
+  svg
+    .append('g')
+    .selectAll('g')
+    .data(data)
+    .enter()
+    .append('g')
+    .each(function (migrantData, i) {
+      const stayDuration = migrantData.Duration;
+
+      // Quadratic function vertex coordinates
+      const h = xScale(stayDuration) / 2;
+      const k = yScale(stayDuration) / 1.5;
+
+      // Quadratic function parameters
+      const a = -k / (h * h),
+        b = (2 * k) / h,
+        c = 0;
+
+      // Get the color based on the date apprehended
+      let dateApprehended = migrantData.DATE_APPREHENDED.split('/');
+      dateApprehendedObj = new Date(
+        dateApprehended[2],
+        dateApprehended[0] - 1,
+        dateApprehended[1]
+      );
+      const color = colors(resolveDateToCategory(dateApprehendedObj));
+
+      // Points generated from quadratic formula
+      const points = createPoints(a, b, c, [0, xScale(stayDuration)], pathStep);
+
+      const arc = d3
+        .select(this)
+        .append('path')
+        .attr('stroke', color)
+        .attr('stroke-width', pathStroke)
+        .attr(
+          'transform',
+          `translate(${marginLeft},${height - marginBottom}) scale(1,-1)`
+        )
+        .attr('fill', 'none');
+
+      // Create the arc animation
+      arc
+        .transition()
+        .delay(pathAnimationDelay(i))
+        .duration(pathAnimationDuration)
+        .ease(d3.easeLinear)
+        .call(animate)
+        // Draw the circle when the animation ends, have it fade in
+        .on('end', () => {
+          d3.select(this)
+            .append('circle')
+            .attr('class', 'control')
+            .attr('r', circleRadius)
+            .attr('cx', marginLeft + xScale(stayDuration))
+            .attr('cy', height - marginBottom)
+            .attr('fill', color)
+            .style('opacity', 0)
+            .transition()
+            .duration(circleAnimationDuration)
+            .ease(d3.easeLinear)
+            .style('opacity', 1);
+          // When the arc animation ends, update the counters
+          const currentTotalDays = parseInt(
+            d3.select('#summary-text-total-days').text().replace(/,/g, '')
+          );
+          const newTotalDays = currentTotalDays + migrantData.Duration;
+          d3.select('#summary-text-total-days').text(commaFormat(newTotalDays));
+          d3.select('#summary-text-total-children').text(commaFormat(i + 1));
+        });
+
+      // Display the tooltip on hover
+      arc
+        .on('mouseover', function (event, d) {
+          d3.select('#tooltip')
+            .style('left', event.pageX + 15 + 'px')
+            .style('top', event.pageY + 'px')
+
+            .classed('hidden', false);
+          d3.select('#value').html(
+            `This child (age <b>${migrantData.AGE}</b>) migrated from <b>${
+              migrantData.COO
+            }</b> and was separated from their family for <b>${commaFormat(
+              migrantData.Duration
+            )}</b> days.`
+          );
+          // Update the arc's styling
+          const thisArc = d3.select(this);
+          thisArc
+            .attr('stroke-width', pathHoverStroke)
+            .style(
+              'filter',
+              `drop-shadow(0px 0px 3px ${thisArc.attr('stroke')})`
+            );
+        })
+        .on('mousemove', (event) => {
+          d3.select('#tooltip')
+            .style('left', event.pageX + 15 + 'px')
+            .style('top', event.pageY + 'px');
+        })
+        .on('mouseout', function () {
+          d3.select('#tooltip').classed('hidden', true);
+          d3.select(this)
+            .attr('stroke-width', pathStroke)
+            .style('filter', 'none');
+        });
+
+      // Animate the arc (taken from https://jsfiddle.net/uw5akb29/)
+      function animate(selection) {
+        selection.attrTween('d', function () {
+          return function (t) {
+            return (
+              'M' +
+              points.slice(0, Math.max(1, (t * points.length) | 0)).join('L')
+            );
+          };
+        });
+      }
+    });
 };
 
 // Load data from CSV and show the bar chart
-d3.csv('test-data.csv', d3.autoType).then((data) => {
-  console.log(data);
-  showCurves(data);
+d3.csv('updated_dataset.csv', d3.autoType).then((data) => {
+  data = data.filter((row) => row.Duration !== 'no discharge');
+
+  const tempFacility = 'BCFS Raymondville';
+  drawArcDiagram(
+    data.filter((record) => record.FACILITY_APPROVED === tempFacility)
+  );
 });
