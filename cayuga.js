b0c5eb1413bfc43b00a9aae707328701645685ad
Sophia Zheng <52471965+sophzheng@users.noreply.github.com>
2021-12-06 10:15:38 -0500

    working legend

--- cayuga.js
+++ cayuga.js
@@ -403,7 +403,7 @@ const drawArcDiagram = (data) => {
  */
 const loadAndDraw = (facility) => {
   // Load data from CSV and show the bar chart
-  d3.csv('arc_data.csv', d3.autoType).then((data) => {
+  d3.csv('updated_dataset.csv', d3.autoType).then((data) => {
     data = data.filter((row) => row.Duration !== 'no discharge');
 
     const tempFacility = facility;
@@ -420,85 +420,13 @@ const loadAndDraw = (facility) => {
 // The value for 'accessToken' begins with 'pk...'
 mapboxgl.accessToken =
   'pk.eyJ1IjoiMTU0LWZhbWlseS1zZXBhcmF0aW9ucyIsImEiOiJja3c4MGhobjJjbW9jMm5xMXNyd21xNXI5In0.hkF5HVL6mdh7v0M0eKaYPg';
-
-
 const map = new mapboxgl.Map({
   container: 'map',
   // Replace YOUR_STYLE_URL with your style URL.
-  style: 'mapbox://styles/154-family-separations/ckwtt4r9i0xbl14pkend62wmv',
-  center: [-95.7129, 36.0902],
+  style: 'mapbox://styles/154-family-separations/ckwa9vc0m5o4y14o3sodg0oaw',
+  center: [-85.7129, 37.0902],
+  //center: [-96, 37.8],
   zoom: 3.5,
-});
-
-map.setMaxBounds(map.getBounds());
-
-map.on('load', function() {
-  map.addLayer(
-    {
-      id: 'country-boundaries',
-      source: {
-        type: 'vector',
-        url: 'mapbox://mapbox.country-boundaries-v1',
-      },
-      'source-layer': 'country_boundaries',
-      type: 'fill',
-      paint: {
-        'fill-color': '#636363',
-        'fill-opacity': 0.2,
-      },
-    },
-  );
-  map.setFilter('country-boundaries', [
-    "in",
-    "iso_3166_1_alpha_3",
-    'USA',
-    'HND', 
-    'SLV',
-    'GTM'
-
-  ]);
-
-
-  const layers = [
-    '0-10',
-    '10-20',
-    '20-50',
-    '50-100',
-    '100-200',
-    '200-500',
-    '500-1000',
-    '1000+'
-  ];
-  const colors = [
-    '#FFEDA0',
-    '#FED976',
-    '#FEB24C',
-    '#FD8D3C',
-    '#FC4E2A',
-    '#E31A1C',
-    '#BD0026',
-    '#800026'
-  ];
-
-    // create legend
-  const legend = document.getElementById('legend');
-  
-  layers.forEach((layer, i) => {
-  const color = colors[i];
-  const item = document.createElement('div');
-  const key = document.createElement('span');
-  key.className = 'legend-key';
-  key.style.backgroundColor = color;
-  
-  const value = document.createElement('span');
-  value.innerHTML = `${layer}`;
-  item.appendChild(key);
-  item.appendChild(value);
-  legend.appendChild(item);
-  });
-
-
-
 });
 
 map.on('mouseover', 'facilities', () => {
@@ -550,19 +478,6 @@ const displaySidebar = (facilityProps, summaryData) => {
     </div>`;
 
   loadAndDraw(facilityProps.FACILITY_APPROVED);
-
-  // Handles closing the sidebar
-  const closeButton = document.getElementById('close-sidebar');
-  closeButton.onclick = () => {
-    mapElement.removeChild(sidebar);
-    map.flyTo({
-      center: [-85.7129, 37.0902],
-      //offset: [-200, 0],
-      zoom: 3.5,
-      speed: 0.75,
-      curve: 1.5,
-    });
-  };
 };
 
 // Actual trigger for loading the sidebar
@@ -573,30 +488,12 @@ map.on('click', 'facilities', function (e) {
 
   const feature = features[0];
 
-  map.flyTo({
-    center: feature.geometry.coordinates,
-    offset: [-500, 0],
-    zoom: 9,
-    speed: 0.75,
-    curve: 1.5,
-  });
-
-  // const popup = new mapboxgl.Popup({
-  //   offset: [0, -15],
-  //   //closeOnClick: true,
-  // })
-  //   .setLngLat(feature.geometry.coordinates)
-  //   .setHTML(
-  //     `<h3>${feature.properties.FACILITY_APPROVED}</h3><p>${feature.properties.count}</p>`
-  //   )
-  //   .addTo(map);
-
   const facilityProps = feature.properties;
 
   if (features.length) {
     //show name and value in sidebar
 
-    d3.csv('map_data.csv', d3.autoType).then((data) => {
+    d3.csv('facility_stats.csv', d3.autoType).then((data) => {
       summary_data = data.find(
         (row) => row.FACILITY_APPROVED === facilityProps.FACILITY_APPROVED
       );
@@ -607,51 +504,4 @@ map.on('click', 'facilities', function (e) {
     const map = document.getElementById('map');
     map.removeChild(document.getElementById('sidebar'));
   }
-});
-
-map.scrollZoom.disable();
-
-var nav = new mapboxgl.NavigationControl({
-  showCompass: false,
-  showZoom: true,
-});
-
-map.addControl(nav, 'top-left');
-
-const layers = [
-  '0-10',
-  '10-20',
-  '20-50',
-  '50-100',
-  '100-200',
-  '200-500',
-  '500-1000',
-  '1000+'
-];
-const colors = [
-  '#FFEDA0',
-  '#FED976',
-  '#FEB24C',
-  '#FD8D3C',
-  '#FC4E2A',
-  '#E31A1C',
-  '#BD0026',
-  '#800026'
-];
-
-// create legend
-const legend = document.getElementById('legend');
-
-layers.forEach((layer, i) => {
-  const color = colors[i];
-  const item = document.createElement('div');
-  const key = document.createElement('span');
-  key.className = 'legend-key';
-  key.style.backgroundColor = color;
-
-  const value = document.createElement('span');
-  value.innerHTML = `${layer}`;
-  item.appendChild(key);
-  item.appendChild(value);
-  legend.appendChild(item);
 });
